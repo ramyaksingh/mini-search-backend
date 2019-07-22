baa07385d822e70dbf4c0ac8c82241e573fe983d
ramyaksingh <singh351@purdue.edu>
2019-07-22 16:23:43 -0400

    DB Fix

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'postgres://umhmrgapsvpbbe:450fd44514f300be53598ab56940059d00e6c885887818957d8b0002b15aa112@ec2-54-83-1-101.compute-1.amazonaws.com:5432/dd81e3rfikijdg'
+#SQLALCHEMY_DATABASE_URI = 'postgres://umhmrgapsvpbbe:450fd44514f300be53598ab56940059d00e6c885887818957d8b0002b15aa112@ec2-54-83-1-101.compute-1.amazonaws.com:5432/dd81e3rfikijdg'
+SQLALCHEMY_DATABASE_URI = 'mysql://b7078f4594c8d2:0fa473d7@us-cdbr-iron-east-02.cleardb.net/heroku_f42847c8e11641b'
